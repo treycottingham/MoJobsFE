68e1debd9655625dc72b6ea67d9a994f28a3c3ba
Andrew Smith <quarsol@yahoo.com>
2018-06-12 14:17:30 -0600

    form fields created

--- mo-jobs-app/src/components/Form.js
+++ mo-jobs-app/src/components/Form.js
@@ -5,13 +5,40 @@ import React, { Component } from 'react'
 class Form extends Component {
   constructor(props) {
     super(props);
-    
+
 }
 
 render(){
 
     return (
-
+      <form action='' method='post'>
+        <label for='POST'>Company Name</label>
+        <input type='text' name='companyName'>
+      </form>
+      <form>
+        <label for='POST'>Resume</label>
+        <input type='checkbox' name='resume'>
+      </form>
+      <form>
+        <label for='POST'>Cover Letter</label>
+        <input type='checkbox' name='coverLetter'>
+      </form>
+      <form>
+        <label for='POST'>Date Applied</label>
+        <input type='date' name='dateApplied'>
+      </form>
+      <form>
+        <label for='POST'>Date of Interview</label>
+        <input type='date' name='doi'>
+      </form>
+      <form>
+        <label for='POST'>Description</label
+        <input type='text' name='description'>
+      </form>
+      <form>
+        <label for='POST'>Required Technologies</label>
+        <input type='text' name='reqTech'>
+      </form>
     )
 }
 

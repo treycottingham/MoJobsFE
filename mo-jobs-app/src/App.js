3a5c98c1c1396e76add91f8386c3f1bcbbe2cc1b
Administrator <adriandlai@gmail.com>
2018-06-12 12:01:12 -0600

    first api working

--- mo-jobs-app/src/App.js
+++ mo-jobs-app/src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import Graph from './components/Graph'
 import CompaniesDash from './components/CompaniesDash'
 
-const apiURL = './jobs.json'
+// const apiURL = './jobs.json'
 
 class App extends Component {
   constructor(props) {
@@ -19,14 +19,25 @@ class App extends Component {
     }
   }
 
+  updateCompany = (company) =>{
+    this.setState(
+      {company}
+    )
+  }
+ 
+
+
+
   toggleHidden() {
     this.setState({
       isHidden: !this.state.isHidden
     })
   }
 
+
   fetchCompany = () => {
-    const apiURL = 'http://localhost:3000/company';
+    // const apiURL = 'http://localhost:3000/company';
+    const apiURL = 'https://mo-jobs-database.herokuapp.com/company';
 
     return fetch(apiURL)
       .then(response => response.json())
@@ -34,7 +45,10 @@ class App extends Component {
         this.setState({company,
                        isCompanyLoaded: true
                         })
-      })
+      console.log('state', this.state.company.company)
+      console.log('true', this.state.isCompanyLoaded)
+      }
+    )
       .catch((err) => console.log('err', err))
   }
 
@@ -55,14 +69,14 @@ class App extends Component {
   // }
 
   render() {
-    const isCompanyLoaded = this.state.CompanyLoaded
+    const isCompanyLoaded = this.state.isCompanyLoaded
     // console.log(this.state)
     return (
       <div className="App">
         <Header />
         <button onClick={(event) => {this.toggleHidden()}}>See Stats</button>
         {this.state.isHidden ? null : <Graph />}
-        {isCompanyLoaded && <CompaniesDash data={this.state.data} />}
+        {isCompanyLoaded && <CompaniesDash updateCompany = {this.updateCompany} data={this.state.company.company} />}
         <Footer />
       </div>
     )
--- mo-jobs-app/src/components/CompaniesDash.js
+++ mo-jobs-app/src/components/CompaniesDash.js
@@ -3,31 +3,89 @@ import React, { Component } from 'react'
 // import JobInfo from './components/JobInfo'
 
 class Companies extends Component {
+  constructor(props) {
+    super(props);
+}
+
+deleteCompany = (e) => {
+  console.log('ids', e)
+  console.log('etarget', e.target)
+  console.log('etarget', e.target.id)
+
+  const url = `http://localhost:3000/reviews/${e.target.id}`
+  fetch(url, {
+    method: 'delete',
+  })
+  .then(resp => resp.json())
+  .then(company => {
+    console.log('didthisdelet?', company) 
+    this.props.updateCart(company)
+    // console.log('allrevies',this.state.allReviews)
+  })
+  .catch(function(error) {
+    console.log('error')
+  })
+  }
+
+
+
+
   render() {
     return (
-      <div>
-        <h1>{this.props.data.map(data => data.company.name)}</h1>
-          <table>
-            <tr>
+    <div> 
+      <table>
+        <tr>
+              <th>Delete Company</th>
               <th>Resume</th>
               <th>Cover Letter</th>
               <th>Date Applied</th>
               <th>Date of Interview</th>
               <th>Description</th>
-            </tr>
-            <tr>
-              <td>{this.props.data.map(data => data.company.resume ? "✓" : "X")}</td>
-              <td>{this.props.data.map(data => data.company.cover ? "✓" : "X")}</td>
-              <td>{this.props.data.map(data => data.company.date_applied)}</td>
-              <td>{this.props.data.map(data => data.company.date_interview)}</td>
-              <td>{this.props.data.map(data => data.company.description)}</td>
-            </tr>
-          </table>
-        <button>Add Job</button>
-        <button>See Stats</button>
+        </tr>
+        {this.props.data.map((data, index) => {
+          return(
+          <tr>
+          <td><button onClick = {this.deleteCompany}>Delete Company</button></td>  
+          <td>{(data.resume ? "✓" : "X")}</td>
+          <td>{(data.cover ? "✓" : "X")}</td>
+          <td>{data.date_applied}</td>
+          <td>{data.interview_date}</td>
+          <td>{data.description}</td>
+        </tr>
+          )
+        }
+      )}
+      </table>
+      <div>
+      <button>Add Job</button>
+      <button>See Stats</button>        
       </div>
+    </div>
+
     )
   }
 }
 
-export default Companies
+export default Companies;
+
+{/*       
+      // <div>
+      //   <h1>{this.props.data.map(data => data.company.name)}</h1>
+      //     <table>
+      //       <tr>
+      //         <th>Resume</th>
+      //         <th>Cover Letter</th>
+      //         <th>Date Applied</th>
+      //         <th>Date of Interview</th>
+      //         <th>Description</th>
+      //       </tr>
+      //       <tr>
+      //         <td>{this.props.data.map(data => data.company.resume ? "✓" : "X")}</td>
+      //         <td>{this.props.data.map(data => data.company.cover ? "✓" : "X")}</td>
+      //         <td>{this.props.data.map(data => data.company.date_applied)}</td>
+      //         <td>{this.props.data.map(data => data.company.date_interview)}</td>
+      //         <td>{this.props.data.map(data => data.company.description)}</td>
+      //       </tr>
+      //      </table>
+        //  <button>Add Job</button>
+        //  <button>See Stats</button> */}

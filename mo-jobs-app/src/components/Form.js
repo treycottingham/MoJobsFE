a26574034260a09d331067219dd173a23efc59b0
Administrator <adriandlai@gmail.com>
2018-06-12 13:39:01 -0600

    added form component

---
+++ mo-jobs-app/src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+// import JobInfo from './components/JobInfo'
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    
+}
+
+render(){
+
+    return (
+
+    )
+}
+
+export default Form;
